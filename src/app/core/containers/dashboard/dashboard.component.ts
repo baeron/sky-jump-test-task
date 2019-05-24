e3f8b5d4ddb102424f200e4ee664918f7a305428
Ievgen <baranoffei@gmail.com>
2019-05-24 10:42:22 +0300

    Add RXJS getting data

--- src/app/core/containers/dashboard/dashboard.component.ts
+++ src/app/core/containers/dashboard/dashboard.component.ts
@@ -2,12 +2,15 @@ import { Component, OnInit } from "@angular/core";
 
 import { Report } from "../../../../../models/report";
 import { ReportService } from "../../services/report.service";
+import { Observable, timer } from 'rxjs';
 @Component({
   selector: "sj-dashboard",
   templateUrl: "./dashboard.component.html",
   styleUrls: ["./dashboard.component.scss"]
 })
 export class DashboardComponent implements OnInit {
+  private _timerStart = 0;
+  private _timerEnd = 15000;
   private _totalReport: Report[];
   constructor(private reportService: ReportService) {}
 
@@ -16,8 +19,11 @@ export class DashboardComponent implements OnInit {
   }
 
   getReport() {
-    this.reportService.getReport().subscribe(report => {
-      this._totalReport = report;
+    timer(this._timerStart, this._timerEnd)
+    .subscribe(() => {
+      this.reportService.getReport().subscribe(report => {
+        this._totalReport = report;
+      });
     });
   }
 }
